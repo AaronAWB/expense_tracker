5460b4a3897e3cd45e19848b8e84cfb3c56bb606
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-10-23 15:28:01 -0700

    add onChange to ExpenseForm

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react';
 const ExpenseForm = () => {
 
     const [description, setDescription] = useState('');
-    const [paymentTpe, setPaymentType] = useState('');
+    const [paymentType, setPaymentType] = useState('');
     const [date, setDate] = useState('');
     const [amount, setAmount] = useState('');
 
@@ -19,11 +19,19 @@ const ExpenseForm = () => {
                         class="form-control" 
                         id="description" 
                         placeholder="Description" 
-                        required>
-                    </input>
+                        required
+                        value={description}
+                        onChange={(event) => setDescription(event.target.value)}
+                    ></input>
                 </div>
 
-                <select class="form-select" aria-label="Default select example" required>
+                <select 
+                    class="form-select" 
+                    aria-label="Default select example" 
+                    required
+                    value={paymentType}
+                    onChange={(event) => setPaymentType(event.target.value)}
+                    >
                     <option selected>Select Payment Type</option>
                     <option value="Card">Card</option>
                     <option value="Cash">Cash</option>
@@ -33,12 +41,25 @@ const ExpenseForm = () => {
 
                 <div className='mb-3'>
                     <label for='date'>Date:</label>
-                    <input type='date' id='date' required></input>
+                    <input 
+                        type='date' 
+                        id='date' 
+                        required
+                        value={date}
+                        onChange={(event) => setDate(event.target.value)}
+                    ></input>
                 </div>
 
                 <div className="mb-3">
                     <label for="amount" class="form-label">Amount</label>
-                    <input type="number" class="form-control" id="amount" placeholder="Amount" required></input>
+                    <input 
+                        type="number" 
+                        class="form-control" 
+                        id="amount" placeholder="Amount" 
+                        required
+                        value={amount}
+                        onChange={(event) => setAmount(event.target.value)}
+                    ></input>
                 </div>
 
             </form>
