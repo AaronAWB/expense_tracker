0c10934042eca89dac10493db9ee5d214b98be78
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-10-23 15:16:40 -0700

    add states to ExpenseForm component

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const ExpenseForm = () => {
+
+    const [description, setDescription] = useState('');
+    const [paymentTpe, setPaymentType] = useState('');
+    const [date, setDate] = useState('');
+    const [amount, setAmount] = useState('');
+
     return (
         <div className='container'>
-            
+
             <form>
 
                 <div className="mb-3">
                     <label for="description" class="form-label">Description</label>
-                    <input type="text" class="form-control" id="description" placeholder="Description"></input>
+                    <input 
+                        type="text" 
+                        class="form-control" 
+                        id="description" 
+                        placeholder="Description" 
+                        required>
+                    </input>
                 </div>
 
-                <select class="form-select" aria-label="Default select example">
-                    <option selected>Open this select menu</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
+                <select class="form-select" aria-label="Default select example" required>
+                    <option selected>Select Payment Type</option>
+                    <option value="Card">Card</option>
+                    <option value="Cash">Cash</option>
+                    <option value="Cryptocurrency">CryptoCurrency</option>
+                    <option value="Other">Other</option>
                 </select>
 
                 <div className='mb-3'>
@@ -25,7 +38,7 @@ const ExpenseForm = () => {
 
                 <div className="mb-3">
                     <label for="amount" class="form-label">Amount</label>
-                    <input type="number" class="form-control" id="amount" placeholder="Amount"></input>
+                    <input type="number" class="form-control" id="amount" placeholder="Amount" required></input>
                 </div>
 
             </form>
