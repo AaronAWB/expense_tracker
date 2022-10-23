3eb79cef81774fa60dc4f72ec64f5de2d051b2e0
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-10-23 14:12:58 -0700

    add descriptionField component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './index.css';
+import DescriptionField from './components/DescriptionField';
+import PaymentTypeField from './components/PaymentTypeField';
+import DateField from './components/DateField';
+import AmountField from './components/AmountField'
 
 function App() {
   return (
     <div className='container'>
       <h1 className='mt-3'>Expense Tracker</h1>
+      <div className='row mt-3'>
+        <div className='col-sm'>
+          <DescriptionField />
+        </div>
+        <div className='col-sm'>
+          <PaymentTypeField />
+        </div>
+      </div>
+      <div className='row mt-3'>
+        <div className='col-sm'>
+          <DateField />
+        </div>
+        <div className='col-sm'>
+          <AmountField />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/DescriptionField.js
+++ src/components/DescriptionField.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 const descriptionField = () => {
     return (
-        
+        <div className="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Description</label>
+            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Description"></input>
+        </div>
     )
 }
+
+export default descriptionField;
