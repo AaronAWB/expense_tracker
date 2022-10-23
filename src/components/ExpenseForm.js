529ca00a70d225bde2b426a39ce98a972b723841
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-10-23 14:53:15 -0700

    add submit button

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -2,31 +2,37 @@ import React from 'react';
 
 const ExpenseForm = () => {
     return (
-        <form>
+        <div className='container'>
+            
+            <form>
 
-            <div className="mb-3">
-                <label for="description" class="form-label">Description</label>
-                <input type="text" class="form-control" id="description" placeholder="Description"></input>
-            </div>
+                <div className="mb-3">
+                    <label for="description" class="form-label">Description</label>
+                    <input type="text" class="form-control" id="description" placeholder="Description"></input>
+                </div>
 
-            <select class="form-select" aria-label="Default select example">
-                <option selected>Open this select menu</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-            </select>
+                <select class="form-select" aria-label="Default select example">
+                    <option selected>Open this select menu</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
 
-            <div className='mb-3'>
-                <label for='date'>Date:</label>
-                <input type='date' id='date' required></input>
-            </div>
+                <div className='mb-3'>
+                    <label for='date'>Date:</label>
+                    <input type='date' id='date' required></input>
+                </div>
 
-            <div className="mb-3">
-                <label for="amount" class="form-label">Amount</label>
-                <input type="number" class="form-control" id="amount" placeholder="Amount"></input>
-            </div>
+                <div className="mb-3">
+                    <label for="amount" class="form-label">Amount</label>
+                    <input type="number" class="form-control" id="amount" placeholder="Amount"></input>
+                </div>
 
-        </form>
+            </form>
+
+            <button type='submit' className='btn btn-primary'>Submit</button>
+
+        </div>
     )
 }
 
