58bad8eacad68bd1411a194365d9b296c6f03cf9
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-10-28 18:27:24 -0700

    add rows and columns to form

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -35,58 +35,66 @@ const ExpenseForm = ({addExpense}) => {
 
             <form onSubmit={handleSubmit}>
 
-            <div className='mb-3 mt-3'>
-                    <label for='date'>Date:</label>
-                    <input 
-                        type='date' 
-                        id='date' 
-                        name='date'
-                        required
-                        value={formData.date}
-                        onChange={handleFormInput}
-                    ></input>
+                <div className='row'>
+
+                    <div className='mb-3 mt-3 col'>
+                        <label for='date'>Date:</label>
+                        <input 
+                            type='date' 
+                            id='date' 
+                            name='date'
+                            required
+                            value={formData.date}
+                            onChange={handleFormInput}
+                        ></input>
+                    </div>
+
+                    <div className='mb-3 col'>
+                        <label for="description" className="form-label">Description</label>
+                        <input 
+                            type="text" 
+                            class="form-control" 
+                            id="description"
+                            name="description"
+                            placeholder="Description" 
+                            required
+                            value={formData.description}
+                            onChange={handleFormInput}
+                        ></input>
+                    </div>
+
                 </div>
 
-                <div className="mb-3">
-                    <label for="description" className="form-label">Description</label>
-                    <input 
-                        type="text" 
-                        class="form-control" 
-                        id="description"
-                        name="description"
-                        placeholder="Description" 
-                        required
-                        value={formData.description}
-                        onChange={handleFormInput}
-                    ></input>
-                </div>
+                <div className='row'>
 
-                <div className="mb-3">
-                    <label for="amount" class="form-label">Amount</label>
-                    <input 
-                        type="number" 
-                        className="form-control" 
-                        id="amount" 
-                        name="amount"
-                        placeholder="Amount" 
-                        required
-                        value={formData.amount}
-                        onChange={handleFormInput}
-                    ></input>
-                </div>
+                    <div className="mb-3 col">
+                        <label for="amount" class="form-label">Amount</label>
+                        <input 
+                            type="number" 
+                            className="form-control" 
+                            id="amount" 
+                            name="amount"
+                            placeholder="Amount" 
+                            required
+                            value={formData.amount}
+                            onChange={handleFormInput}
+                        ></input>
+                    </div>
 
-                <div className="mb-3">
-                    <label for="location" className="form-label">Location</label>
-                    <input 
-                        type="text" 
-                        class="form-control" 
-                        id="location"
-                        name="location"
-                        placeholder="Location" 
-                        required
-                        value={formData.location}
-                        onChange={handleFormInput}
-                    ></input>
+                    <div className="mb-3 col">
+                        <label for="location" className="form-label">Location</label>
+                        <input 
+                            type="text" 
+                            class="form-control" 
+                            id="location"
+                            name="location"
+                            placeholder="Location" 
+                            required
+                            value={formData.location}
+                            onChange={handleFormInput}
+                        ></input>
+                    </div>
+
                 </div>
 
                 <div className='text-center mb-3'>
