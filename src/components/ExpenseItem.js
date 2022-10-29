fc4c831263c0117ff3665e61e9a1a22d04782c47
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-10-29 16:02:53 -0700

    deconstruct ExpenseItem props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const ExpenseItem = (props) => {
+const ExpenseItem = ({ id, date, description, amount, location, handleDelete }) => {
     return (
-        <tr key={props.id}>
-            <td width='20%'>{props.date}</td>
-            <td width='20%'>{props.description}</td>
-            <td width='20%'>${props.amount}</td>
-            <td width='20%'>{props.location}</td>
+        <tr key={id}>
+            <td width='20%'>{date}</td>
+            <td width='20%'>{description}</td>
+            <td width='20%'>${amount}</td>
+            <td width='20%'>{location}</td>
             <td width='20%'>
                  <button 
                     type='button' 
                     className='btn btn-outline-danger btn-sm' 
-                    onClick={() => props.handleDelete(props.id)}>
+                    onClick={() => handleDelete(id)}>
                     Delete
                 </button>
             </td>
