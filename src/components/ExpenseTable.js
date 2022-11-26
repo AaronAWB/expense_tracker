06f40187cfc0c96620a549aeacce5f21a7242f9f
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-11-26 13:42:07 -0800

    refactor rendering of table rows in ExpenseTable

--- src/components/ExpenseTable.js
+++ src/components/ExpenseTable.js
@@ -2,6 +2,27 @@ import React from 'react';
 import ExpenseItem from './ExpenseItem';
 
 const ExpenseTable = ({expenses, handleDelete}) => {
+
+    const renderTableRow = () => {
+        
+        if (expenses.length === 0) {
+        return(
+            <tr>
+                <td colSpan={5}>No expenses added yet...</td>
+            </tr>
+        )}
+
+        return expenses.map((expense)=>(
+                <ExpenseItem 
+                id={expense.id}
+                date={expense.date}
+                description={expense.description}         
+                amount={expense.amount} 
+                location={expense.location}
+                handleDelete={handleDelete}
+                />
+        ));    
+    }
     
     return (
         <div className='container mt-4 border rounded expense-table shadow p-3 mb-5'>
@@ -17,16 +38,7 @@ const ExpenseTable = ({expenses, handleDelete}) => {
                         </tr>
                     </thead>
                     <tbody className='text-center'>
-                        {expenses.map(((expense)=>(
-                            <ExpenseItem 
-                            id={expense.id}
-                            date={expense.date}
-                            description={expense.description}         
-                            amount={expense.amount} 
-                            location={expense.location}
-                            handleDelete={handleDelete}
-                            />
-                        )))}
+                        {renderTableRow()}
                     </tbody>
                 </table>
             </div>
